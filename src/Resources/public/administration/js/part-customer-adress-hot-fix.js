8c30e2848b449a8d9c71beda5f8ee33d5ee6433e
uwe <u.mayer@part.berlin>
2020-04-14 13:00:25 +0200

    add admin build to repo

---
+++ src/Resources/public/administration/js/part-customer-adress-hot-fix.js
@@ -0,0 +1,1 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([["part-customer-adress-hot-fix"],{RuY1:function(s,e,t){"use strict";t.r(e);t("emib")},emib:function(s,e){Shopware.Component.override("sw-customer-detail-addresses",{methods:{isValidAddress:s=>!0}})}},[["RuY1","runtime"]]]);
